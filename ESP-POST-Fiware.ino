58112ece54f246d4a94baf2862f72df0ac00e5ee
Ana Bheatriz Chacon <anabheatrizchacon@outlook.com>
2019-10-21 22:17:11 -0300

    correção

--- ESP-POST-Fiware.ino
+++ ESP-POST-Fiware.ino
@@ -1,13 +1,15 @@
-#include <ArduinoJson.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
 #include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h> 
+#include <ArduinoJson.h>
 
-const char* wifiName = "Fiware Test";
-const char* wifiPass = "Fiware12345";     // The password of the Wi-Fi network
+const char* wifiName     = "Bia";        
+const char* wifiPass = "12345678"; 
 
-const char* host= "http://192.168.0.32:1026/v2/entities";
-int cont = 001;
+const char* host= ".http://localhost:1026/v2/entities";
+
+char idNode[80];
+char nomeNode[12] = "Temperatura";
+int cont = 2;
 
 void setup() {
 
@@ -20,60 +22,67 @@ void setup() {
   WiFi.begin(wifiName, wifiPass); 
   
   int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Aguarde o Wi-Fi se conectar
+  while (WiFi.status() != WL_CONNECTED) { 
     delay(1000);
     Serial.print(++i); Serial.print(' ');
   }
-
+  
   Serial.println("");
   Serial.println("Wi-Fi connected");  
   Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP()); // Envie o endereço IP do ESP8266 para o computador
+  Serial.println(WiFi.localIP()); 
+  
 }
 
 void loop() {
   
-  if (WiFi.status() == WL_CONNECTED){ //Check WiFi connection status
+  if (WiFi.status() == WL_CONNECTED){ 
     
 //#####################################################################################################   
     //JSON
-    StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-    JsonObject& root = JSONbuffer.createObject(); 
-    
-    root["id"] = "urn:ngsi-ld:Temperatura:"+cont;
-    cont++;
+ 
+    sprintf(idNode, "urn:ngsi-ld:%s:%03i", nomeNode, cont++);
+
+    StaticJsonBuffer<300> JSONbuffer;
+    JsonObject& root = JSONbuffer.createObject();
+
+    root["id"] = idNode;
     root["type"] = "Temperatura";
+
     
     JsonObject& name = root.createNestedObject("name");
-    name["type"] = "Float";
-    name["value"] = "1.0";
+    name["type"] = "Text";
+    name["value"] = "Temperatura";
+
+    JsonObject& temp = root.createNestedObject("temp");
+    temp["type"] = "Float";
+    temp["value"] = "23.1";
     
     JsonObject& refSensor = root.createNestedObject("refSensor");
     refSensor["type"] = "Relationship";
     refSensor["value"] = "urn:ngsi-ld:Sensor:001";
-    
-    root.printTo(Serial);
-//#####################################################################################################
 
     char JSONmessageBuffer[300];
     root.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
     Serial.println(JSONmessageBuffer);
     
-    HTTPClient http;    //Declare object of class HTTPClient 
+//#####################################################################################################
+   
+    HTTPClient http;
    
     Serial.print("Request Link:"); 
     Serial.println(host); 
    
-    http.begin(host);     //Specify request destination 
-    http.addHeader("Content-Type", "application/json"); //Specify content-type header
+    http.begin(host);   
+    http.addHeader("Content-Type", "application/json"); 
     
-    int httpCode = http.POST(JSONmessageBuffer); //Send the request
-    String payload = http.getString(); //Get the response payload
+    int httpCode = http.POST(JSONmessageBuffer); 
+    String payload = http.getString(); 
     
-    Serial.println(httpCode); //Print HTTP return code
-    Serial.println(payload); //Print request response payload
+    Serial.println(httpCode); 
+    Serial.println(payload); 
     
-    http.end(); //Close connection
+    http.end(); 
     
   }else{
     
@@ -81,22 +90,7 @@ void loop() {
     
   }
  
-  delay(30000);  //Send a request every 30 seconds
+  delay(30000);
  
-}  
-
-
-
-/*{
-        "id":"urn:ngsi-ld:Temperatura:001", 
-        "type":"Temperatura",
-        "name":{
-            "type":"Text", 
-            "value":"Temperatura"
-        },
-        "refSensor": {
-            "type": "Relationship",
-            "value": "urn:ngsi-ld:Sensor:001"
-        }
-  }*/
+}
    
